2703a4b415c87d92e2a52acbd5d86147a5943ab8
Luke James <james.luke318@gmail.com>
2013-06-21 17:20:50 -0400

    added jquery library

---
+++ js/jquery/noconflict.js
@@ -0,0 +1,5 @@
+if(window.devicePixelRatio)
+{
+	document.write("<script type=\"text/css\">button.form-button span, button.form-button-alt span {margin: -1px 0px 0px 0px !important;}</script>");
+}
+jQuery.noConflict();	
